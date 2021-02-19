a5459c45f1dd9f93a3c2dcf41a8ea61e186cc68a
Rachel <rachel17.22@gmail.com>
2021-02-18 22:08:22 -0700

    placeholder setup for navbar, content, and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
 import './App.css';
+
 
 function App() {
   return (
     <div>
-      <h1>Content will go here</h1>
+      <div className="navbar bg-warning">
+        <h1>Navbar</h1>
+      </div>
+
+      <div className="container">
+        <h1>Content!</h1>
+        <p>There will be content and things here. For now, just some simple text</p>
+      </div>
+
+      <div className="card-footer">
+        <small>Footer placeholder</small>
+      </div>
     </div>
   );
 }
