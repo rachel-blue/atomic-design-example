9006b42cf7a0a8708cc9ef1ab56766b098577fd2
Rachel <rachel17.22@gmail.com>
2021-02-23 09:49:26 -0700

    kitten js object created as local mock for api data and mapped to display on page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { kittens } from './kittens.js';
 import './App.css';
 
-
 function App() {
+  console.log(kittens)
+
   return (
     <div>
       <div className="navbar navbar-light bg-light">
@@ -15,69 +17,27 @@ function App() {
 
         <div className="row">
 
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3 py-2">
-            <div className="card">
-              <img
-                className="card-img-top"
-                src="https://cdn.pixabay.com/photo/2018/07/13/10/20/cat-3535404_960_720.jpg"
-                alt="Card image cap"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+          {
+            kittens.map((item) => (
+              <div
+                className="col-12 col-sm-6 col-md-4 col-lg-3 py-2"
+                key={item.title}
+              >
+                <div className="card">
+                  <img
+                    className="card-img-top"
+                    src={item.image}
+                    alt="Card image cap"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p className="card-text">{item.description}</p>
+                    <a href="#" className="btn btn-primary">Go Somewhere</a>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3 py-2">
-            <div className="card">
-              <img
-                className="card-img-top"
-                src="https://cdn.pixabay.com/photo/2018/07/13/10/20/cat-3535404_960_720.jpg"
-                alt="Card image cap"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3 py-2">
-            <div className="card">
-              <img
-                className="card-img-top"
-                src="https://cdn.pixabay.com/photo/2018/07/13/10/20/cat-3535404_960_720.jpg"
-                alt="Card image cap"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-12 col-sm-6 col-md-4 col-lg-3 py-2">
-            <div className="card">
-              <img
-                className="card-img-top"
-                src="https://cdn.pixabay.com/photo/2018/07/13/10/20/cat-3535404_960_720.jpg"
-                alt="Card image cap"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
+            ))
+          }
 
         </div>
 
---
+++ src/kittens.js
@@ -0,0 +1,28 @@
+import kitteh from './images/kittenPics/kitteh.jpg'
+import pounce from './images/kittenPics/pounce.jpg'
+import mittens from './images/kittenPics/mittens.jpg'
+import bartholomew from './images/kittenPics/bartholomew.jpg'
+
+
+export const kittens = [
+  {
+    title: "Kitteh Meow-Face",
+    description: "Lorem ipsum dolor sit amet asdlkfj asdjfhaslkf alskjf ajdflakjsdf ljsdfljasdf jasfd ja",
+    image: kitteh
+  },
+  {
+    title: "Sir Pounce-a-lot",
+    description: "Lorem ipsum dolor sit amet asdlkfj asdjfhaslkf alskjf ajdflakjsdf ljsdfljasdf jasfd ja",
+    image: pounce
+  },
+  {
+    title: "Detective Mittens",
+    description: "Lorem ipsum dolor sit amet asdlkfj asdjfhaslkf alskjf ajdflakjsdf ljsdfljasdf jasfd ja",
+    image: mittens
+  },
+  {
+    title: "BartholoMew",
+    description: "Lorem ipsum dolor sit amet asdlkfj asdjfhaslkf alskjf ajdflakjsdf ljsdfljasdf jasfd ja",
+    image: bartholomew
+  }
+]
