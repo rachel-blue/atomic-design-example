c69ef54f78d301d7e8d14db1771e25c4be92c47a
rachel-blue <53097494+rachel-blue@users.noreply.github.com>
2021-02-24 13:38:58 -0700

    One File Dump (#1)
    * placeholder setup for navbar, content, and footer
    
    * placeholder kitten cards
    
    * kitten js object created as local mock for api data and mapped to display on page
    
    * router and about page added, about page placeholder content from Cat Ipsum generator
    
    * styling navbar and edited kitten array to extend content
    
    * button meow alert on click and cleaned up footer and readme
    
    * added more kitten assets and changed from browser router to hash router

---
+++ src/kittens.js
@@ -0,0 +1,87 @@
+import kitteh from './images/kittenPics/kitteh.jpg'
+import pounce from './images/kittenPics/pounce.jpg'
+import mittens from './images/kittenPics/mittens.jpg'
+import bartholomew from './images/kittenPics/bartholomew.jpg'
+import pickle from './images/kittenPics/pickle.jpg'
+import wiggums from './images/kittenPics/wiggums.jpg'
+import stitch from './images/kittenPics/stitch.jpg'
+import salem from './images/kittenPics/salem.jpg'
+import congress from './images/kittenPics/congress.jpg'
+import buckwheat from './images/kittenPics/buckwheat.jpg'
+import goose from './images/kittenPics/goose.jpg'
+import whiskers from './images/kittenPics/whiskers.jpg'
+
+export const kittens = [
+  {
+    title: "Kitteh Meow-Face",
+    id: "kitteh-meow-face",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: kitteh,
+  },
+  {
+    title: "Sir Pounce-a-lot",
+    id: "sir-pounce-a-lot",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: pounce,
+  },
+  {
+    title: "Detective Mittens",
+    id: "detective-mittens",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: mittens,
+  },
+  {
+    title: "BartholoMew",
+    id: "bartholomew",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: bartholomew,
+  },
+  {
+    title: "Pickle",
+    id: "pickle",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: pickle,
+  },
+  {
+    title: "Mr. Wiggums",
+    id: "mr-wiggums",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: wiggums,
+  },
+  {
+    title: "Stitch",
+    id: "stitch",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: stitch,
+  },
+  {
+    title: "Salem",
+    id: "salem",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: salem,
+  },
+  {
+    title: "Congress",
+    id: "congress",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: congress,
+  },
+  {
+    title: "Buckwheat",
+    id: "buckwheat",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: buckwheat,
+  },
+  {
+    title: "Goose",
+    id: "goose",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: goose,
+  },
+  {
+    title: "Whiskers",
+    id: "whiskers",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris augue dui, tempus",
+    image: whiskers,
+  },
+];
